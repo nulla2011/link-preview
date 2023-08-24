a20524498efa2a311a0f305bdcb8587b7b43978c
nulla2011 <nulla@outlook.com>
2023-08-24 22:23:08 +0800

    chore: asset prefix

--- vite.config.ts
+++ vite.config.ts
@@ -4,6 +4,16 @@ import react from '@vitejs/plugin-react';
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
+  build: {
+    rollupOptions: {
+      output: {
+        // eslint-disable-next-line @typescript-eslint/no-unused-vars
+        assetFileNames: (assetInfo) => 'assets/link-preview-[name]-[hash][extname]',
+        entryFileNames: 'assets/link-preview-[name]-[hash].js',
+        chunkFileNames: 'assets/link-preview-[name]-[hash].js',
+      },
+    },
+  },
   // resolve: {
   //   alias: {
   //     '~bootstrap': path.resolve(__dirname, 'node_modules/bootstrap'),
