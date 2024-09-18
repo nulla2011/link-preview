9dbce170621cd282bf33d87f9619fd13345f860f
nulla2011 <nulla@outlook.com>
2024-09-18 21:41:23 +0800

    add license; add description

--- README.md
+++ README.md
@@ -1,1 +1,3 @@
 对磁链等进行一个预览
+
+使用了 [whatslink.info](https://whatslink.info/) 的 api
--- src/App.tsx
+++ src/App.tsx
@@ -11,8 +11,15 @@ function App() {
   return (
     <>
       <div className="header-wrapper">
-        <h1>Link Preview</h1>
+        <h1 className="my-4">Link Preview</h1>
         <p>Basic information for Torrent/Magnet/Ed2k link</p>
+        <p className="fw-light" style={{ fontSize: '0.9rem' }}>
+          Just another instance of{' '}
+          <a href="https://whatslink.info/" target="_blank">
+            whatslink.info
+          </a>{' '}
+          api
+        </p>
       </div>
       <div className="search-wrapper">
         <Search onSearch={handleSearch} />
